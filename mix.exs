6d15a4d0b4bfe307848dceb648df0e570dd16a40
Kerry Buckley <kerryjbuckley@gmail.com>
2024-09-14 15:42:48 +0100

    Relax minimum Elixir version to 1.13

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule WxExCompiler.MixProject do
     [
       app: :wx_ex_compiler,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
