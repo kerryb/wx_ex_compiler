3b5236e8d1866202e8844b0642a63204586d6016
Kerry Buckley <kerryjbuckley@gmail.com>
2024-09-19 15:29:32 +0100

    Bump version to 0.4.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule WxExCompiler.MixProject do
   def project do
     [
       app: :wx_ex_compiler,
-      version: "0.4.0",
+      version: "0.4.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
